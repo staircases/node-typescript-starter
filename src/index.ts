b7a7fe004d9fe7add68092036e540e02160eb4ad
CCS\calxa <xaviermbcalabia@gmail.com>
2023-06-19 11:34:09 +0800

    added module

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
-console.log('hi')
-const arr: number[] = [0, 1, 2]
-for (const i of arr) {
-    console.log(i)
+import * as fs from 'fs'
+
+function blocking(file: string) {
+    console.log('before read sync')
+    const data = fs.readFileSync(file)
+    console.log('after read sync', data.toString())
+    console.log('end')
+}
+
+function nonBlocking(file: string) {
+    console.log('before read async')
+    fs.readFile(file, (err, data) => {
+        if (err) {
+            console.error('error reading file', err)
+            return
+        }
+        console.log('message', data.toString())
+    })
+    console.log('after read async')
+    console.log('end')
 }
