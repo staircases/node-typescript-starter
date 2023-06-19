26e1c330603fc8645b5fba62fd270c7c5473d044
CCS\calxa <xaviermbcalabia@gmail.com>
2023-06-19 13:08:43 +0800

    express

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import express, { application, Express, Request, Response } from 'express'
 
-const all: Express = express()
+const app: Express = express()
 const port = 5555
 
-application.listen(port, () => {
+app.get('/', (re: Request, resp: Response) => {
+    resp.send('Express + Typescript')
+})
+
+app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`)
 })
