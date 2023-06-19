ca3c2e92f46e07234be3f69795fcacff99f56922
CCS\calxa <xaviermbcalabia@gmail.com>
2023-06-19 15:07:49 +0800

    remove application

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { application, Express, Request, Response } from 'express'
+import express, { Express, Request, Response } from 'express'
 import environment from './environment'
 import * as routes from './routes'
 
