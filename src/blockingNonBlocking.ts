07afa31c275f2feeb1d84c4eefceea679df6b6c9
CCS\calxa <xaviermbcalabia@gmail.com>
2023-06-19 11:42:41 +0800

    rename module

--- src/blockingNonBlocking.ts
+++
@@ -1,1 +0,0 @@
-import * as fn from './blockingNonBlocking'
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,5 @@
-import * as fs from 'fs'
+import { blocking, nonBlocking } from './filedemo'
 
-function blocking(file: string) {
-    console.log('before read sync')
-    const data = fs.readFileSync(file)
-    console.log('after read sync', data.toString())
-    console.log('end')
-}
-
-function nonBlocking(file: string) {
-    console.log('before read async')
-    fs.readFile(file, (err, data) => {
-        if (err) {
-            console.error('error reading file', err)
-            return
-        }
-        console.log('message', data.toString())
-    })
-    console.log('after read async')
-    console.log('end')
-}
+const filename = '/Users/calxa/Documents/test.txt'
+blocking(filename)
+nonBlocking(filename)
