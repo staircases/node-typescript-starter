94465ee38327bb9aba18c262e61f3a3a72cc0af0
CCS\calxa <xaviermbcalabia@gmail.com>
2023-06-19 11:05:45 +0800

    initial commit

---
+++ build/index.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log("hi");
+const arr = [0, 1, 2];
+for (const i of arr) {
+    console.log(i);
+}
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+console.log('hi')
+const arr: number[] = [0, 1, 2]
+for (const i of arr) {
+    console.log(i)
+}
