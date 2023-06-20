a9952b72809b8e45dc3a000af4044ae324817612
staircases <xaviermbcalabia@gmail.com>
2023-06-20 13:58:37 +0800

    mariadb

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,12 @@ import express, { Express } from 'express'
 import environment from './environment'
 import createServer from './server'
 import SequelizeConnection from './database/configuration'
+import { db } from './database/models'
 ;(async () => {
     await SequelizeConnection.connect()
+    db.sequelize.sync({
+        force: true,
+    })
 })()
 
 const app: Express = express()
