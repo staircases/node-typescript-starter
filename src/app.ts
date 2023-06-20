a6f2ddb6246059247c3b6270d5558db799501082
staircases <xaviermbcalabia@gmail.com>
2023-06-20 13:49:00 +0800

    mariadb

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,17 @@
-import express, { Express, Request, Response } from 'express'
+import express, { Express } from 'express'
 import environment from './environment'
 import createServer from './server'
+import SequelizeConnection from './database/configuration'
+;(async () => {
+    await SequelizeConnection.connect()
+})()
 
 const app: Express = express()
 const appPort = environment.APP_PORT
 
 createServer(app).routes().errorHandler().start(appPort)
+
+process.on('SIGINT', () => {
+    SequelizeConnection.close()
+    process.exit()
+})
