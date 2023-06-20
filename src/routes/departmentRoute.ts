ebdd3472354510f61235ed3108369023de4bd4d8
staircases <xaviermbcalabia@gmail.com>
2023-06-20 15:28:49 +0800

    routes

--- src/routes/departmentRoute.ts
+++ src/routes/departmentRoute.ts
@@ -1,12 +1,42 @@
-import express, { Request, Response } from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import DepartmentService from '../services/departmentService'
 
 const router = express.Router()
 
-router.get('/', async (req: Request, resp: Response, next) => {
+router.get('/', async (req: Request, resp: Response, next: NextFunction) => {
     try {
         const departments = await DepartmentService.getInstance().findAll()
         resp.status(200).json(departments)
+    } catch (e) {
+        next(e)
+    }
+})
+
+router.get('/:id', async (req: Request, resp: Response, next: NextFunction) => {
+    try {
+        const existingDepartment =
+            await DepartmentService.getInstance().findById(
+                parseInt(req.params.id)
+            )
+        if (existingDepartment) {
+            resp.status(200).json(existingDepartment)
+        } else {
+            resp.status(404).json({
+                message: `Department ${req.params.id} not found.`,
+            })
+        }
+    } catch (e) {
+        next(e)
+    }
+})
+
+router.post('/', async (req: Request, resp: Response, next: NextFunction) => {
+    try {
+        const payload = { ...req.body }
+        const newDepartment = await DepartmentService.getInstance().save(
+            payload
+        )
+        resp.status(201).json({ ...newDepartment.dataValues })
     } catch (e) {
         next(e)
     }
